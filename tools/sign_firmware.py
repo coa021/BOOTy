537a1b85aba8e715b80a34d3e670391b7d8f62ec
coa021 <coa2102001@gmail.com>
2026-05-04 15:41:35 +0200

    feat(bl): add ecdsa
    along this i have also separated log functionality into separate module. my py script is now padding 80 bytes. bootloader size is 32KB, i have done this because of sector alignment. i tried flashing 24KB bootloader and it works, but as soon as i flash header and app after the bootloader, nothing starts. i think i cant write different apps in same sector, im not sure about this. i tried aligning with different paddings, but it didnt work, i tried 0x100, 0x1000, 0x200, divisible with 256, 512, nope not working. i have copied crypto stuff from tinycrypt, i just needed ecdsa and sha256, still it takes a lot of space. i think thats all for this commit, i mightve missed something

--- tools/sign_firmware.py
+++ tools/sign_firmware.py
@@ -3,12 +3,31 @@ import argparse
 import struct
 import sys
 from pathlib import Path
-import zlib
+import zlib # for crc
+import hashlib # for sha256
+from cryptography.hazmat.primitives.asymmetric import ec, utils
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric.utils import decode_dss_signature
 
-# Must match shared/app_header.h
-APP_MAGIC    = 0x0B00B1E5
-HEADER_SIZE  = 16384
 
+APP_MAGIC = 0x0B00B1E5
+HEADER_SIZE = 80
+
+def load_private_key():
+    with open("dont-touch/private_key.pem", "rb") as f:
+        return serialization.load_pem_private_key(f.read(), password=None)
+
+def sign_code(code, private_key):
+    sig_der = private_key.sign(code, ec.ECDSA(hashes.SHA256()))
+    r, s = decode_dss_signature(sig_der)
+    return r.to_bytes(32, 'big') + s.to_bytes(32, 'big')
+
+def custom_sha256(data):
+    digest = hashlib.sha256(data).digest()
+    print(f"digest: {digest}")
+    print(f"digest hex: {digest.hex()}")
+
+    return digest
 
 def custom_crc(app_bin):
     with open(app_bin, "rb") as f:
@@ -30,9 +49,16 @@ def cmd_sign(args):
     version = version_to_int(args.version)
 
     crc = custom_crc(args.input)
+    custom_sha256(code)
 
+    private_key = load_private_key()
+    signature = sign_code(code, private_key)
+    print(f"Signature: {signature.hex()}")
 
     header = struct.pack("<IIII", APP_MAGIC, version, len(code), crc)
+
+    header += signature 
+
     header += bytes(HEADER_SIZE - len(header))
     
     assert len(header) == HEADER_SIZE
@@ -43,7 +69,7 @@ def cmd_sign(args):
     
     print(f"Signed image: {out}  ({len(header) + len(code)} bytes)")
     print(f"  Header @ 0x08004000 ({HEADER_SIZE} bytes)")
-    print(f"  Code   @ 0x08008000 ({len(code)} bytes)")
+    print(f"  Code   @ 0x08006000 ({len(code)} bytes)")
 
 
 if __name__ == "__main__":
